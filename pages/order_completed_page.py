97138d760ec8c4da803d5cfe872953754a578411
Elis Kik <kik_elis@mail.ru>
2023-04-15 17:25:51 +0300

    Fix regex for order ID

--- pages/order_completed_page.py
+++ pages/order_completed_page.py
@@ -1,11 +1,10 @@
-from re import match
+from re import search
 
 import allure
 
 from selenium.webdriver import Firefox as WebDriver
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 class OrderCompletedPage:
@@ -13,7 +12,7 @@ class OrderCompletedPage:
     container_caption = [By.XPATH, './/div[starts-with(@class, "Order_Text")]']
     button_status = [By.XPATH, './/button[text() = "Заказать"]']
 
-    pattern_order_id = r':\s+(\d+)\.'
+    pattern_order_id = r':\s+([^.]+)'
 
     def __init__(self, webdriver: WebDriver, wait: WebDriverWait):
         self.webdriver = webdriver
@@ -22,7 +21,7 @@ class OrderCompletedPage:
     @allure.step('Получение ID заказа')
     def get_order_id(self) -> str | None:
         element = self.webdriver.find_element(*self.container_caption)
-        order_id_match = match(self.pattern_order_id, element.text)
+        order_id_match = search(self.pattern_order_id, element.text)
 
         if order_id_match is not None:
             return order_id_match.group(1)
@@ -32,4 +31,4 @@ class OrderCompletedPage:
     @allure.step('Проверка присутствия ID заказа на странице')
     def check_has_order_id(self):
         element = self.webdriver.find_element(*self.container_caption)
-        assert match(self.pattern_order_id, element.text) is not None, 'Order ID was not found'
+        assert search(self.pattern_order_id, element.text) is not None, 'Order ID was not found'
